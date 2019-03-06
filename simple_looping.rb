6c3f763a3a37fc8bb6fddeb25f67eedc257fa587
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-06 21:01:10 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def times_iterator(number_of_times)
   number_of_times.times do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
-end
+  end
     
 
 def while_iterator(number_of_times)
@@ -23,19 +23,19 @@ def while_iterator(number_of_times)
   while counter <= number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
-end
-end
+  end
+  end
 
 def until_iterator(number_of_times)
   counter = 0
 until counter == number_of_times
 puts "Welcome to Flatiron School's Web Development Course!"
 counter += 1
-end
-end
+  end
+  end
 
 def for_iterator(number_of_times)
   for number in 0..number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
-end
-end
+  end
+  end
